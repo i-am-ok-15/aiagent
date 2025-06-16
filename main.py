ef20269368c0ec4e9f6d814e1112d24b7996ca45
rtasker15 <rtasker15@gmail.com>
2025-06-16 19:53:28 +0100

    added argv variable

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import os
+import sys
+
 from dotenv import load_dotenv
 from google import genai
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
-
 client = genai.Client(api_key=api_key)
 
-response = client.models.generate_content(model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+prompt_text = sys.argv[1]
 
+print(prompt_text)
 
-print(f"Your response: {response.text}")
+def gemini_prompt(prompt_text):
+    if len(prompt_text) > 0:
+        response = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt_text)
+        print(f"Your response: {response.text}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    else:
+        print("Error, invalid prompt")
+        sys.exit(1)
+
+gemini_prompt(prompt_text)
+
