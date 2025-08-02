66f6d8567483469c2fc3dc08f335f0d8972bb168
rtasker15 <rtasker15@gmail.com>
2025-08-02 21:30:33 +0100

    second commit

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
-def main():
-    print("Hello from aiagent!")
+import os
+import sys
+from dotenv import load_dotenv
 
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+from google import genai
+from google.genai import types
+
+
+client = genai.Client(api_key=api_key)
+
+input = sys.argv
+user_prompt = sys.argv[1]
+
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
+
+def main():
+    
+    response = client.models.generate_content(
+    model='gemini-2.0-flash-001', contents=messages
+    )
+    
+    if "--verbose" in sys.argv:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    
+    print(response.text)
 
 if __name__ == "__main__":
     main()
