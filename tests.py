7bf7bbd399c17b937f791d39aa1a6699f71ed822
rtasker15 <rtasker15@gmail.com>
2025-08-02 23:18:36 +0100

    Ch2, L2 -completed

---
+++ tests.py
@@ -0,0 +1,13 @@
+from functions.get_files_info import get_files_info
+
+print(get_files_info("calculator", ".") + "\n")
+
+print(get_files_info("calculator", "pkg") + "\n")
+
+print(get_files_info("calculator", "/bin") + "\n")
+
+print(get_files_info("calculator", "../") + "\n")
+
+
+
+
