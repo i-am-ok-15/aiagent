2a7acdb9bb4a46706cc54043db35ae5b1dc97b34
rtasker15 <rtasker15@gmail.com>
2025-08-10 21:57:33 +0100

    ai agent, CH2, L4 completed

---
+++ functions/write_file.py
@@ -0,0 +1,23 @@
+import os
+
+def write_file(working_directory, file_path, content):
+    
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+
+    try:
+        if not abs_file_path.startswith(abs_working_dir):
+            return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+        
+        dir_name = os.path.dirname(abs_file_path)
+
+        if dir_name:
+            os.makedirs(dir_name, exist_ok=True)
+
+            with open(abs_file_path, "w", encoding="utf-8") as f:
+                f.write(content)
+
+            return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+            
+    except Exception as error:
+        return f"Error: unable to write to file {error}"
