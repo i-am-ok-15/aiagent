139b374dad4cbc959a1bc6bb5d86e7c7a4bd485d
rtasker15 <rtasker15@gmail.com>
2025-08-12 11:09:24 +0100

    ai agent, ch 3 l3, complete

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ api_key = os.environ.get("GEMINI_API_KEY")
 from google import genai
 from google.genai import types
 
+from functions.get_files_info import schema_get_files_info
+
 client = genai.Client(api_key=api_key)
 
 
@@ -18,6 +20,12 @@ def main():
     input = sys.argv
     user_prompt = sys.argv[1]
 
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+        ]
+    )
+
     messages = [
     types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
@@ -25,7 +33,10 @@ def main():
     response = client.models.generate_content(
         model='gemini-2.0-flash-001', 
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=SYSTEM_PROMPT),
+        config=types.GenerateContentConfig(
+            tools=[available_functions], 
+            system_instruction=SYSTEM_PROMPT
+        )
     )
     
     if "--verbose" in sys.argv:
@@ -33,6 +44,11 @@ def main():
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     
+    if response.function_calls:
+        for function_call_part in response.function_calls:
+            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+
+    print("Response:")
     print(response.text)
 
 if __name__ == "__main__":
